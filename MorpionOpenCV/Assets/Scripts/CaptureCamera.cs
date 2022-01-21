f1875b5855b043a78c503546367dfbcc3b5822b7
EleouetClement <eleouet.clement@outlook.fr>
2022-01-21 09:37:16 +0100

    ajouts plugins

---
+++ MorpionOpenCV/Assets/Scripts/CaptureCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
+public class CaptureCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
